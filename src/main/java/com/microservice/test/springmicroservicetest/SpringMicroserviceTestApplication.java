2bf0a95c39af7d0fab03518dd0d20503f469d037
Amar Gupta <apap.gupta@gmail.com>
2020-06-04 00:56:10 +0530

    first commit

---
+++ src/main/java/com/microservice/test/springmicroservicetest/SpringMicroserviceTestApplication.java
@@ -0,0 +1,31 @@
+package com.microservice.test.springmicroservicetest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class SpringMicroserviceTestApplication {
+
+	@Autowired
+	RestTemplate restTemplate;
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceTestApplication.class, args);
+	}
+	@GetMapping("/testService")
+	public String testService() {
+		return this.restTemplate.getForObject("http://localhost:9092/getServerInfo", String.class);
+	}
+	 @Bean
+	 RestTemplate restTemplate(){
+	    return new RestTemplate();
+	 }
+	
+}
---
+++ src/test/java/com/microservice/test/springmicroservicetest/SpringMicroserviceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.test.springmicroservicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
